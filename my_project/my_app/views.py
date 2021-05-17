fc64b2369c99b9139608ec1eecc604535c1ea912
trl242 <royaurallockhart@gmail.com>
2021-05-16 22:13:06 -0700

    added class feature to models.py and feature to index.html

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'fast'
+    feature1.details = 'Our service is really quick.'
+    return render(request, 'index.html', {'feature': feature1})
 
 def counter(request):
     text = request.POST['text']
