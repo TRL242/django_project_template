3e0d287ed57609d184f3de8295c43644e348006e
trl242 <royaurallockhart@gmail.com>
2021-05-19 11:17:21 -0700

    added more features to views then added those views to index.html

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -7,9 +7,25 @@ from .models import Feature
 def index(request):
     feature1 = Feature()
     feature1.id = 0
-    feature1.name = 'fast'
+    feature1.name = 'Fast'
     feature1.details = 'Our service is really quick.'
-    return render(request, 'index.html', {'feature': feature1})
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Reliable'
+    feature2.details = 'Our service is very reliable.'
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Easy To Use'
+    feature3.details = 'Our service is easy to use.'
+
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'Affordable'
+    feature4.details = 'Our service is very affordable.'
+
+    return render(request, 'index.html', {'feature1': feature1, 'feature2': feature2, 'feature3': feature3, 'feature4': feature4})
 
 def counter(request):
     text = request.POST['text']
