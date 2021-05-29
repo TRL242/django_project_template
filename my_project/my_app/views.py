cb0b3434ba85a93c33e4c287132b10035318b981
trl242 <royaurallockhart@gmail.com>
2021-05-29 15:50:06 -0700

    added boolean for features

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -8,26 +8,31 @@ def index(request):
     feature1 = Feature()
     feature1.id = 0
     feature1.name = 'Fast'
+    feature1.is_true = True
     feature1.details = 'Our service is really quick.'
 
     feature2 = Feature()
     feature2.id = 1
     feature2.name = 'Reliable'
+    feature2.is_true = True
     feature2.details = 'Our service is very reliable.'
 
     feature3 = Feature()
     feature3.id = 2
     feature3.name = 'Easy To Use'
+    feature3.is_true = True
     feature3.details = 'Our service is easy to use.'
 
     feature4 = Feature()
     feature4.id = 3
     feature4.name = 'Affordable'
+    feature4.is_true = True
     feature4.details = 'Our service is very affordable.'
 
     feature5 = Feature()
     feature5.id = 4
     feature5.name = 'Trustworthy'
+    feature5.is_true = False
     feature5.details = 'Service you can trust.'
 
     features = [feature1, feature2, feature3, feature4, feature5]
