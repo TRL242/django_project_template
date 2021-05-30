8912487a20b728fc3627b7e7e93f4e9607c6dceb
trl242 <royaurallockhart@gmail.com>
2021-05-30 07:07:49 -0700

    created register, and login in views and html

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -1,45 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 
 # Create your views here.
 def index(request):
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Fast'
-    feature1.is_true = True
-    feature1.details = 'Our service is really quick.'
-
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Reliable'
-    feature2.is_true = False
-    feature2.details = 'Our service is very reliable.'
-
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Easy To Use'
-    feature3.is_true = True
-    feature3.details = 'Our service is easy to use.'
-
-    feature4 = Feature()
-    feature4.id = 3
-    feature4.name = 'Affordable'
-    feature4.is_true = True
-    feature4.details = 'Our service is very affordable.'
-
-    feature5 = Feature()
-    feature5.id = 4
-    feature5.name = 'Trustworthy'
-    feature5.is_true = False
-    feature5.details = 'Service you can trust.'
-
-    features = [feature1, feature2, feature3, feature4, feature5]
-    for feature in features:
-        pass
-
+    features = Feature.objects.all()
     return render(request, 'index.html', {'features': features})
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.object.filter(email=email).exists():
+                messages.info(request, 'Email Already Used')
+                return redirect('register')
+            elif User.object.filter(username=username).exists():
+                messages.info(request, 'Username Already exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Password not the same')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
+    return render(request, 'register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
 
 def counter(request):
     text = request.POST['text']
