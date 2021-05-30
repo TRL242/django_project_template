ca6b840c5b2be589da65614eafe7b2cb51cce254
trl242 <royaurallockhart@gmail.com>
2021-05-30 14:11:57 -0700

    added post

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -54,8 +54,15 @@ def login(request):
     else:
         return render(request, 'login.html')
 
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
 
 def counter(request):
     text = request.POST['text']
     amount_of_words = len(text.split())
     return render(request, 'counter.html', {'amount': amount_of_words})
+
+def post(request, pk):
+    return render(request, 'post.html', {'pk': pk})
